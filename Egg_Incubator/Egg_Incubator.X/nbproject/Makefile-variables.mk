4b943b910937efe953e311ec31f38f367f9211aa
Minh-Khanh Ngo <55183891+KhanhNgo19@users.noreply.github.com>
2021-03-28 15:50:03 +0700

    Add files via upload

---
+++ Egg_Incubator/Egg_Incubator.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Egg_Incubator.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Egg_Incubator.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=eggincubator.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/eggincubator.x.tar
